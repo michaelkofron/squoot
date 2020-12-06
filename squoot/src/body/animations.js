e8865d1e6ea2cbeac519e583fd2f0dda8d5db6e4
Michael Kofron <kofronmichael@gmail.com>
2020-12-06 16:26:57 -0600

    animation change

--- squoot/src/body/animations.js
+++ squoot/src/body/animations.js
@@ -2,9 +2,17 @@
 
 let mobile = (thisReplace) => {
 
-    thisReplace.setState({width: 360, height: 620})
+    let interval = setInterval(() => {
+        if (thisReplace.state.width > 360){
+            thisReplace.setState({width: thisReplace.state.width - 10})
+        } else {
+            clearInterval(interval)
+        }
+        
+    }, 10)
+    
 
-
+    //thisReplace.setState({width: 360, height: 620})
 }
 
 let desktop = (thisReplace) => {
